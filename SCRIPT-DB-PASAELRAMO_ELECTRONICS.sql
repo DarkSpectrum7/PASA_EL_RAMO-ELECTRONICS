359b60d9a58019bb31476335a8d9ac6796a27df5
DarkSpectrum7 <68966844+DarkSpectrum7@users.noreply.github.com>
2020-11-14 18:07:53 -0300

    UODATE-SCRIPT-141120201807

--- SCRIPT-DB-PASAELRAMO_ELECTRONICS.sql
+++ SCRIPT-DB-PASAELRAMO_ELECTRONICS.sql
@@ -1,6 +1,7 @@
 create database pasaelramo_electronics
-
+go
 use pasaelramo_electronics
+go
 --==========================================================================--
 --LOGIN
 --==========================================================================--
@@ -10,61 +11,26 @@ create table login (id int,
 					clave varchar (100),
 					primary key (usuario))
 go
---==========================================================================--
---Documento
---==========================================================================--
-create table Documento (	
-							id_doc int identity (1,1) not null, -- PK
-							id_empresa int  not null, -- Fk
-							id_det int, -- FK -- detalle
-							total_neto numeric,
-							total_iva numeric,
-							total_bruto numeric,
-							anulada numeric,
-							emitida numeric,
-							fecha varchar(100),
-							primary key (id_doc),
-							foreign key(id_det) references detalle(id_det),
-							foreign key(id_empresa) references empresa(id_empresa))
-
-
-go
-----==========================================================================--
-----Tipo_documento
-----==========================================================================--
---create table Tipo_documento (	id int,
---								id_doc int identity (1,1), ---pk
---								tipo char,
---								primary key (id_doc));
 
 --==========================================================================--
---DETALLE
+--CLIENTE
 --==========================================================================--
-create table detalle (	id int,
-						id_det int identity (1,1), -- PK
-						id_prod int , --FK -- producto
-						cantidad varchar(100),
-						id_doc int , --FK -- factura
-						primary key (id_det),
-						FOREIGN KEY (id_prod) references producto(id_prod))
-
+create table cliente (	id_cli int identity (1,1), -- PK
+						rut varchar(12),
+						giro varchar(100),
+						ciudad varchar(100),
+						comuna varchar(100),
+						direccion varchar(100),
+						telefono varchar(100),
+						email varchar(100),
+						referencia varchar(100),
+						primary key (id_cli))
 
 go
 --==========================================================================--
---PRODUCTO
+--EMPRESA
 --==========================================================================--
-create table producto ( id int,
-						id_prod int identity (1,1), --PK
-						nombre_prod varchar(12),
-						precio_uni varchar(100),
-						primary key (id_prod))
-
-go
---==========================================================================--
---Empresa
---==========================================================================--
-create table empresa ( id int,
-						id_empresa int identity (1,1), -- PK
+create table empresa (	id_empresa int identity (1,1), -- PK
 						rut varchar(12),
 						giro varchar(100),
 						ciudad varchar(100),
@@ -76,5 +42,51 @@ create table empresa ( id int,
 						primary key (id_empresa))
 
 go
+--==========================================================================--
+--PRODUCTO
+--==========================================================================--
+create table producto ( id_prod int identity (1,1), --PK
+						nombre_prod varchar(12),
+						precio_uni varchar(100),
+						primary key (id_prod))
+
+go
+--==========================================================================--
+--DETALLE
+--==========================================================================--
+create table detalle (	id_det int identity (1,1), -- PK
+						id_prod int , --FK -- PRODUCTO
+						cantidad varchar(100),
+						id_doc int , --FK -- DOCUMENTO
+						primary key (id_det),
+						FOREIGN KEY (id_prod) references producto(id_prod))
+
+
+go
+
+--==========================================================================--
+--DOCUMENTO
+--==========================================================================--
+create table documento (	id_doc int identity (1,1) not null, -- PK
+							id_empresa int  not null, -- Fk -- EMPRESA
+							id_cli int not null, -- FK -- CLIENTE
+							id_det int not null, -- FK -- DETALLE
+							total_neto numeric (10),
+							total_iva numeric(10),
+							total_bruto numeric (10),
+							anulada char (1),
+							emitida char (1),
+							fecha datetime,
+							primary key (id_doc),
+							foreign key(id_det) references detalle(id_det),
+							foreign key(id_empresa) references empresa(id_empresa),
+							foreign key(id_cli) references cliente(id_cli))
+
+
+go
+
+--==========================================================================--
+--
+--==========================================================================--
 
 alter table detalle add FOREIGN KEY (id_doc) references documento(id_doc)
